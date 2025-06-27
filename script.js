38581dbcc80fee4fdfb68052bd0c2b6553787b50
Kyle-858 <krazykyle19@gmail.com>
2025-06-27 18:45:02 -0400

    Finalized Project Recent

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function openMenu() {
-    document.body.classList.add("menu--open");
+    indexe.body.classList.add("menu--open");
 }
 
 function closeMenu() {
     document.body.classList.remove("menu--open");
 }
+
+const emailInput = document.querySelector(".header__email--input");
+
+function adjustPlaceholder() {
+    const emailInput = document.querySelector(".header__email--input");
+    if (window.innerWidth < 768) {
+        emailInput.placeholder = "Email";
+    } else {
+        emailInput.placeholder = "Your E-mail Here"
+    }
+}
+
+// Run on page
+window.addEventListener("load", adjustPlaceholder);
+window.addEventListener("resize", adjustPlaceholder);
